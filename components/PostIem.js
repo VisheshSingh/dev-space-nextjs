9d96565444fb5d0e39299750ed543c6f1efcf2ce
Vishesh Singh <VisheshSingh16@gmail.com>
2021-06-09 12:32:23 -0500

    Get Posts & Parse Frontmatter

---
+++ components/PostIem.js
@@ -0,0 +1,5 @@
+const PostIem = () => {
+  return <div className='rounded-lg p-5 shadow m-6'>hello</div>;
+};
+
+export default PostIem;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-import Image from 'next/image';
+import fs from 'fs';
+import path from 'path';
+import marked from 'marked';
+import matter from 'gray-matter';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
+import PostIem from '../components/PostIem';
 
-export default function HomePage() {
-  return <Layout>Hello world</Layout>;
+export default function HomePage({ posts }) {
+  return (
+    <Layout>
+      {posts.map((post) => (
+        <PostIem key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
 }
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((file) => {
+    const slug = file.replace('.md', '');
+    const markdownWithMeta = fs.readFileSync(path.join('posts', file), 'utf-8');
+    const { data: frontmatter } = matter(markdownWithMeta);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts: posts.slice(0, 6),
+    },
+  };
+};
